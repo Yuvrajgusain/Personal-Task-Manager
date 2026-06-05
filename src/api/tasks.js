442916d0868989b6f899352196d99230f0e71c1b
Yuvrajgusain <yuvrajgusain10@gmail.com>
2026-06-05 16:49:21 +0530

    Connect frontend to deployed backend

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,4 +1,4 @@
-const BASE = 'http://localhost:3001/api';
+const BASE = 'https://taskmanagerbackend-zjdt.onrender.com/api';
 
 async function request(path, options = {}) {
     const res = await fetch(`${BASE}${path}`, {
